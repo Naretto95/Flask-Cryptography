65ff2da2f66940ab6b427980795698bedca839e4
Naretto95 <narettolilian@hotmail.fr>
2025-11-25 23:19:14 +0100

    optimize functions

--- src/crypt_function.py
+++ src/crypt_function.py
@@ -1,15 +1,22 @@
-import os 
+import os
 import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import base64
 import qrcode
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from shutil import copyfile
 from src.stegano import *
 from pyzbar import pyzbar
 import time
 from asn1crypto import tsp
+
+# Path constants
+CERT_PATH = os.path.join('ressources', 'cert', 'cert.pem')
+PRIVATE_KEY_PATH = os.path.join('ressources', 'private', 'key.pem')
+DIPLOMAS_DIR = os.path.join('ressources', 'Diplomas')
+ASSETS_DIR = os.path.join('ressources', 'assets')
+EMPTY_DIPLOMA_PATH = os.path.join(ASSETS_DIR, 'empty_diploma.png')
+FONT_PATH = os.path.join(ASSETS_DIR, 'AlgerianRegular.ttf')
 
 
 def encrypt_private_key(a_message, private_key):
@@ -24,15 +31,15 @@ def decrypt_public_key(encoded_encrypted_msg, public_key):
     decoded_decrypted_msg = encryptor.decrypt(decoded_encrypted_msg)
     return decoded_decrypted_msg
 
-def sign(token):   
-    with open(os.getcwd()+os.path.sep+'ressources'+os.path.sep+"cert"+os.path.sep+"cert.pem","r") as certif:
+def sign(token):
+    with open(os.path.join(os.getcwd(), CERT_PATH), "r") as certif:
         public_key = RSA.importKey(certif.read())
-    return encrypt_private_key(token,public_key)
+    return encrypt_private_key(token, public_key)
 
 def verify(encrypt_message):
-    with open(os.getcwd()+os.path.sep+'ressources'+os.path.sep+'private'+os.path.sep+'key.pem','r') as f :
-        private_keys = RSA.importKey(f.read(),passphrase="keepbreathing")
-    return decrypt_public_key(encrypt_message,private_keys)
+    with open(os.path.join(os.getcwd(), PRIVATE_KEY_PATH), 'r') as f:
+        private_keys = RSA.importKey(f.read(), passphrase="keepbreathing")
+    return decrypt_public_key(encrypt_message, private_keys)
 
 def generate_qrcode(secret_data, path,id_diploma):
     token = sign(secret_data)
@@ -51,33 +58,33 @@ def generate_qrcode(secret_data, path,id_diploma):
     img_qr.thumbnail((350,350))
     qr_pos = (diploma.size[0]-360,diploma.size[1]-360)
     diploma.paste(img_qr,qr_pos)
-    diploma.save(os.getcwd()+os.path.sep+'ressources'+os.path.sep+"Diplomas"+os.path.sep+"diploma_"+str(id_diploma)+'.png')
+    diploma.save(os.path.join(os.getcwd(), DIPLOMAS_DIR, f"diploma_{id_diploma}.png"))
 
 def generate_unique_diploma(user, diploma):
     """print Name, diploma and years on diploma and make some steganography to 
     transform standard picture to a unique one"""
 
-    copyfile(os.path.join(os.getcwd(), 'ressources', 'assets', 'empty_diploma.png'), "temp.png")
+    copyfile(os.path.join(os.getcwd(), EMPTY_DIPLOMA_PATH), "temp.png")
     img = Image.open('temp.png')
     ts = str(time.time())
-    secret_data = user.first_name + user.name + diploma.specialisation + user.school + str(diploma.graduation_years) + ts
+    secret_data = user.first_name + user.name + diploma.specialization + user.school + str(diploma.graduation_year) + ts
     secret_data = secret_data + '.' * (64 - len(secret_data))
-    empty_dip =  Image.open("temp.png")
+    empty_dip = Image.open("temp.png")
     d = ImageDraw.Draw(empty_dip)
-    fnt = ImageFont.truetype(os.path.join(os.getcwd(), 'ressources', 'assets', 'AlgerianRegular.ttf'), 50)
+    fnt = ImageFont.truetype(os.path.join(os.getcwd(), FONT_PATH), 50)
     pos_user = ((empty_dip.size[0]//2-200,empty_dip.size[1]//2-100))
     pos_diploma=((empty_dip.size[0]//2-200,empty_dip.size[1]//2-50))
     pos_years = ((empty_dip.size[0]//2-100,empty_dip.size[1]//2))
     user_draw  = user.first_name +' '+user.name
-    diploma_draw = diploma.specialisation +' '+user.school
+    diploma_draw = diploma.specialization +' '+user.school
     d.text(pos_user,user_draw,fill=(0,0,0),font=fnt)
     d.text(pos_diploma,diploma_draw,fill=(0,0,0),font=fnt)
-    d.text(pos_years,str(diploma.graduation_years),fill=(0,0,0),font=fnt)
+    d.text(pos_years,str(diploma.graduation_year),fill=(0,0,0),font=fnt)
     empty_dip.save("ready_to_qr.png")
-    generate_qrcode(secret_data,'ready_to_qr.png',diploma._id)
-    img=Image.open(os.path.join(os.getcwd(), 'ressources', 'Diplomas', f"diploma_{diploma._id}.png"))
-    cacher(img,secret_data)
-    img.save(os.path.join(os.getcwd(), 'ressources', 'Diplomas', f"diploma_{diploma._id}.png"))
+    generate_qrcode(secret_data,'ready_to_qr.png',diploma.id)
+    img = Image.open(os.path.join(os.getcwd(), DIPLOMAS_DIR, f"diploma_{diploma.id}.png"))
+    cacher(img, secret_data)
+    img.save(os.path.join(os.getcwd(), DIPLOMAS_DIR, f"diploma_{diploma.id}.png"))
     os.remove("temp.png")
     os.remove("ready_to_qr.png")
     os.remove('qr_temp.png')
--- src/totp.py
+++ src/totp.py
@@ -1,51 +1,64 @@
 import pyotp
 import os
-import string
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import qrcode
 
-def sendMail(to:string):
-    otp = pyotp.TOTP('base32secret3232').provisioning_uri(name='admin@cytech.eu', issuer_name='CY Diploma')
-    qr = qrcode.make(otp)
-    qr.save(os.path.join(os.getcwd(), 'ressources', 'assets', 'qrcode.png'))
-    otp = pyotp.TOTP('base32secret3232')
+# Constants
+TOTP_SECRET = 'base32secret3232'
+EMAIL_FROM = 'cyu.otpdiploma@gmail.com'
+EMAIL_PASSWORD = 'admincyuniversite'
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+QR_PATH = os.path.join('ressources', 'assets', 'qrcode.png')
+
+def sendMail(to: str):
+    otp_uri = pyotp.TOTP(TOTP_SECRET).provisioning_uri(name='admin@cytech.eu', issuer_name='CY Diploma')
+    qr = qrcode.make(otp_uri)
+    qr.save(os.path.join(os.getcwd(), QR_PATH))
+    otp = pyotp.TOTP(TOTP_SECRET)
     msg = MIMEMultipart()
-    msg['From'] = 'cyu.otpdiploma@gmail.com'
+    msg['From'] = EMAIL_FROM
     msg['To'] = to
     msg['Subject'] = 'Your OTP QRCODE'
-    with open(os.path.join(os.getcwd(), 'ressources', 'assets', 'qrcode.png'), 'rb') as f:
+    with open(os.path.join(os.getcwd(), QR_PATH), 'rb') as f:
         img_data = f.read()
     image = MIMEImage(img_data, name="OTP.png")
     msg.attach(image)
-    s = smtplib.SMTP("smtp.gmail.com",587)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login('cyu.otpdiploma@gmail.com', 'admincyuniversite')
-    s.sendmail(to, to, msg.as_string())
-    s.quit()
+    try:
+        s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(EMAIL_FROM, EMAIL_PASSWORD)
+        s.sendmail(EMAIL_FROM, to, msg.as_string())
+        s.quit()
+    except Exception as e:
+        print(f"Error sending email: {e}")
     return otp
 
-def verifyotp(token):
-    otp = pyotp.TOTP('base32secret3232')
+def verifyotp(token: str) -> bool:
+    otp = pyotp.TOTP(TOTP_SECRET)
     return otp.verify(token)
 
-def maildiploma(diploma,to):
+def maildiploma(diploma: str, to: str):
     with open(diploma, 'rb') as f:
         img_data = f.read()
     message = MIMEMultipart()
-    message['From'] = "cyu.otpdiploma@gmail.com"
+    message['From'] = EMAIL_FROM
     message['To'] = to
     message['Subject'] = "Your diploma"
     image = MIMEImage(img_data, name="Diploma.png")
     message.attach(image)
-    s = smtplib.SMTP("smtp.gmail.com",587)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login('cyu.otpdiploma@gmail.com', 'admincyuniversite')
-    s.sendmail(to, to, message.as_string())
-    s.quit()
+    try:
+        s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(EMAIL_FROM, EMAIL_PASSWORD)
+        s.sendmail(EMAIL_FROM, to, message.as_string())
+        s.quit()
+    except Exception as e:
+        print(f"Error sending diploma email: {e}")
 
